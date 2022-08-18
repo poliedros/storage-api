351abab31206e6fe54b59000c74cbaa93ee19360
Carlos <carloshlzansavio@gmail.com>
2022-08-18 16:01:33 +0100

    feat: add configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { StorageModule } from './storage/storage.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { ItemsModule } from './items/items.module';
+import { StorageModule } from './storage/storage.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     StorageModule,
     ItemsModule,
