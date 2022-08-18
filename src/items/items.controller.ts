253a227fd90ba79146bc15659dc01a102879d128
Carlos <carloshlzansavio@gmail.com>
2022-08-18 16:17:52 +0100

    change controller to listen to redis

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,41 +1,34 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Body, Patch, Param, Delete } from '@nestjs/common';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 
-@Controller('items')
+@Controller()
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
-  @Post()
+  @EventPattern({ items: 'create' })
   create(@Body() createItemDto: CreateItemDto) {
     return this.itemsService.create(createItemDto);
   }
 
-  @Get()
+  @MessagePattern({ items: 'findall' })
   findAll() {
     return this.itemsService.findAll();
   }
 
-  @Get(':id')
+  @MessagePattern({ items: 'find' })
   findOne(@Param('id') id: string) {
     return this.itemsService.findOne(id);
   }
 
-  @Patch(':id')
+  @EventPattern({ items: 'patch' })
   update(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto) {
     return this.itemsService.update(id, updateItemDto);
   }
 
-  @Delete(':id')
+  @EventPattern({ items: 'remove' })
   remove(@Param('id') id: string) {
     return this.itemsService.remove(id);
   }
--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,41 +1,34 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Body, Param } from '@nestjs/common';
 import { StorageService } from './storage.service';
 import { CreateStorageDto } from './dto/create-storage.dto';
 import { UpdateStorageDto } from './dto/update-storage.dto';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller('storage')
 export class StorageController {
   constructor(private readonly storageService: StorageService) {}
 
-  @Post()
+  @EventPattern({ storage: 'create' })
   create(@Body() createStorageDto: CreateStorageDto) {
     return this.storageService.create(createStorageDto);
   }
 
-  @Get()
+  @MessagePattern({ storage: 'findall' })
   findAll() {
     return this.storageService.findAll();
   }
 
-  @Get(':id')
+  @MessagePattern({ storage: 'find' })
   findOne(@Param('id') id: string) {
     return this.storageService.findOne(id);
   }
 
-  @Patch(':id')
+  @EventPattern({ storage: 'patch' })
   update(@Param('id') id: string, @Body() updateStorageDto: UpdateStorageDto) {
     return this.storageService.update(id, updateStorageDto);
   }
 
-  @Delete(':id')
+  @EventPattern({ storage: 'delete' })
   remove(@Param('id') id: string) {
     return this.storageService.remove(id);
   }
