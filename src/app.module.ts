e487aff95984db4090b04a238951576beed8cabf
Carlos <carloshlzansavio@gmail.com>
2022-08-18 15:56:08 +0100

    add mongoose env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
-/* import { AppController } from './app.controller';
-import { AppService } from './app.service'; */
 import { StorageModule } from './storage/storage.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://amendes:EoyOR1Wxi9penzYH@cluster0.59uij.mongodb.net/test',
-    ),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     StorageModule,
     ItemsModule,
-  ],
-  controllers: [
-    /* AppController */
-  ],
-  providers: [
-    /* AppService */
   ],
 })
 export class AppModule {}
